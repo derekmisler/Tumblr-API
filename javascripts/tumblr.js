062ed8f4c21e47176f64a44228ced8f30b7f7227
derekmisler <derekmisler@gmail.com>
2015-11-05 11:46:29 -0500

    layout need a fixin'

--- javascripts/tumblr.js
+++ javascripts/tumblr.js
@@ -3,13 +3,14 @@ $.ajax({
 	dataType: 'jsonp',
 	success: function(info){
 		console.log('Tumblr API success');
-		$("#tumblr h1").html(info.response.blog.title);
+		$("#tumblr h2").html(info.response.blog.title);
 	}
 });
 $.ajax({
 	url: "http://api.tumblr.com/v2/blog/montanamoment.tumblr.com/posts?api_key=v6y8zw1aQk66fJevXGoFyv3BHfURcphvUD6hl88BB3yVyLKVm6",
 	dataType: 'jsonp',
 	success: function(posts){
+		console.log('Tumblr API image success');
 		var postings = posts.response.posts;
 		var text = '';
 		for (var i in postings) {
@@ -17,11 +18,11 @@ $.ajax({
 			var image = p.photos[0].alt_sizes[2];
 			text += '<li><a href="' + p.post_url + '" target="_blank"><img class="" src=' + image.url +'></a></li>';
 		}
-		$('#tumblr ul').append(text);
+		$('#tumblr .block-grid').append(text);
 	}
 });
 $(function() {
-	$('#tumblr img').each(function(i) {
+	$('#tumblr').find('img').each(function(i) {
 		$(this).delay((i++) * 100).fadeTo(200, 1);
 	});
 });
