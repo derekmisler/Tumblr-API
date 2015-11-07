09fb317a0e7ebc6daa8d5b1da49dd1c998ff59a6
derekmisler <derekmisler@gmail.com>
2015-11-06 19:26:33 -0500

    deleting what i don't need

--- README.md
+++
@@ -1,3 +0,0 @@
-## API example for PR team for pulling in user-generated content
-
-Check it out here: http://derekmisler.github.io/Tumblr-API/
--- javascripts/tumblr.js
+++ javascripts/tumblr.js
@@ -1,28 +1,30 @@
 $.ajax({
-	url: "http://api.tumblr.com/v2/blog/montanamoment.tumblr.com/info?api_key=v6y8zw1aQk66fJevXGoFyv3BHfURcphvUD6hl88BB3yVyLKVm6",
-	dataType: 'jsonp',
-	success: function(info){
-		console.log('Tumblr API success');
-		$("#tumblr h2").html(info.response.blog.title);
-	}
+  url: "http://api.tumblr.com/v2/blog/montanamoment.tumblr.com/info?api_key=v6y8zw1aQk66fJevXGoFyv3BHfURcphvUD6hl88BB3yVyLKVm6",
+  dataType: 'jsonp',
+  success: function(info){
+    console.log('title success');
+    $("#tumblr .title").html(info.response.blog.title);
+  }
 });
 $.ajax({
-	url: "http://api.tumblr.com/v2/blog/montanamoment.tumblr.com/posts?api_key=v6y8zw1aQk66fJevXGoFyv3BHfURcphvUD6hl88BB3yVyLKVm6",
-	dataType: 'jsonp',
-	success: function(posts){
-		console.log('Tumblr API image success');
-		var postings = posts.response.posts;
-		var text = '';
-		for (var i in postings) {
-			var p = postings[i];
-			var image = p.photos[0].alt_sizes[2];
-			text += '<li><a href="' + p.post_url + '" target="_blank"><img class="" src=' + image.url +'></a></li>';
-		}
-		$('#tumblr .block-grid').append(text);
-	}
+  url: "http://api.tumblr.com/v2/blog/montanamoment.tumblr.com/posts?api_key=v6y8zw1aQk66fJevXGoFyv3BHfURcphvUD6hl88BB3yVyLKVm6",
+  dataType: 'jsonp',
+  success: function(posts){
+    console.log('image success');
+    var postings = posts.response.posts;
+    var text = '';
+    for (var i in postings) {
+      var p = postings[i];
+      var image = p.photos[0].alt_sizes[2];
+      text += '<li><a href="' + p.post_url + '" target="_blank"><img class="" src=' + image.url +'></a></li>';
+    }
+    $('#tumblr ul').append(text);
+    $(function() {
+      $('#tumblr').find('img').each(function(i) {
+        $(this).delay((i++) * 100).fadeTo(200, 1);
+      });
+    });
+
+  }
 });
-$(function() {
-	$('#tumblr').find('img').each(function(i) {
-		$(this).delay((i++) * 100).fadeTo(200, 1);
-	});
-});
+
